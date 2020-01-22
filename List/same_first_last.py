67fa93a0fa0adcb285331ef436d4ecc471235bd7
Victor-Onyejide <59766880+Victor-Onyejide@users.noreply.github.com>
2020-01-22 11:11:29 -0500

    Update same_first_last.py

--- List/same_first_last.py
+++ List/same_first_last.py
@@ -1,3 +1,12 @@
+
+# Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
+
+
+# same_first_last([1, 2, 3]) → False
+# same_first_last([1, 2, 3, 1]) → True
+# same_first_last([1, 2, 1]) → True
+
+#Solution
 def same_first_last(nums):
   length = len(nums)
   # last = nums[len(nums)-1]
