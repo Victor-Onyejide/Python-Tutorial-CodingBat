be5d30b7f36944d2f55e13a4bb24bcf161aa359f
Victor-Onyejide <59766880+Victor-Onyejide@users.noreply.github.com>
2020-01-22 11:07:49 -0500

    Update first_last6.py

--- List/first_last6.py
+++ List/first_last6.py
@@ -1,3 +1,11 @@
+# Given an array of ints, return True if the array is length 1 or more, and the first element and the last element are equal.
+
+
+# same_first_last([1, 2, 3]) → False
+# same_first_last([1, 2, 3, 1]) → True
+# same_first_last([1, 2, 1]) → True
+
+#Solution
 def first_last6(nums):
   if nums[0] == 6:
     return True
